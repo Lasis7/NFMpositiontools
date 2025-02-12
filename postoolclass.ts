b6c63e1ecf979dffd96afd7686a819e48f1fe0e6
Lasis7 <AE8656@student.jamk.fi>
2025-02-13 00:11:59 +0200

    changed how parseLine returns the values

--- postoolclass.ts
+++ postoolclass.ts
@@ -1,22 +1,46 @@
 import * as fs from 'fs';
 
 export function parseLine(codeline: string) {
+  const codeObj: { [key: string]: number } = {
+    pieceNumber: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+    angle: 0,
+  };
   const numbers = codeline.match(/-?\d+/g);
   const xyz = numbers?.map((i) => Number(i));
-  return xyz;
-  // if (numbers?.length === 4) {
-  //   const indexes = [1, 2];
-  //   const xy: number[] = indexes.map((i) => Number(numbers[i]));
-  //   xy.push(0);
-  //   return xy;
-  // } else if (numbers?.length === 5) {
-  //   const indexes = [1, 2, 3];
-  //   const xyz: number[] = indexes.map((i) => Number(numbers[i]));
-  //   return xyz;
-  // } else {
-  //   return -1;
-  // }
+  if (xyz?.length === 4) {
+    Object.keys(codeObj).forEach((key, index) => {
+      if (key !== 'z' && key !== 'angle') {
+        codeObj[key] = xyz[index];
+      } else if (key === 'angle') {
+        codeObj[key] = xyz[index - 1];
+      }
+    });
+    return codeObj;
+  } else if (xyz?.length === 5) {
+    Object.keys(codeObj).forEach((key, index) => {
+      codeObj[key] = xyz[index];
+    });
+    return codeObj;
+  } else {
+    return -1;
+  }
 }
+
+// if (numbers?.length === 4) {
+//   const indexes = [1, 2];
+//   const xy: number[] = indexes.map((i) => Number(numbers[i]));
+//   xy.push(0);
+//   return xy;
+// } else if (numbers?.length === 5) {
+//   const indexes = [1, 2, 3];
+//   const xyz: number[] = indexes.map((i) => Number(numbers[i]));
+//   return xyz;
+// } else {
+//   return -1;
+// }
 
 export function generateSpike(
   codeline: string,
@@ -27,30 +51,22 @@ export function generateSpike(
   if (amount < 0) {
     return console.log('Amount needs to be a positive number');
   } else {
-    let pieceNumber: number = 0;
-    let x: number = 0;
-    let y: number = 0;
-    let z: number = 0;
-    let angle: number = 0;
+    let pieceNumber = 0;
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    let angle = 0;
     const xyz = parseLine(codeline);
-    if (xyz?.length === 4) {
-      pieceNumber = xyz[0];
-      x = xyz[1];
-      y = xyz[2];
-      angle = xyz[3];
-    } else if (xyz?.length === 5) {
-      pieceNumber = xyz[0];
-      x = xyz[1];
-      y = xyz[2];
-      z = xyz[3];
-      angle = xyz[4];
-    } else {
+    if (xyz === -1) {
       console.log('invalid line');
       return;
+    } else {
+      pieceNumber = xyz.pieceNumber;
+      x = xyz.x;
+      y = xyz.y;
+      z = xyz.z;
+      angle = xyz.angle;
     }
-
-    // let y_updated = y;
-    // let z_updated = z;
     switch (dir) {
       case 'x':
         fs.appendFileSync('code.txt', `Generated ${amount} spikes:\n`);
